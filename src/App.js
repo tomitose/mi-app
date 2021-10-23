7a45624644e9cc297cdf3f84192df9200a90ca1e
tomitose <tomitosello@gmail.com>
2021-10-23 18:09:26 -0300

    imagen de logocarrito

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from './components/NavBar.js'
+// import cartlogo from '../public/cartlogo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar/>
+        {/* <img src={cartlogo} alt="logo" /> */}
       </header>
 
     </div>
