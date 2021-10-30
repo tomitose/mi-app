9523c518c3942995244d428217bb0b90202f419b
tomitose <tomitosello@gmail.com>
2021-10-30 10:43:41 -0300

    pequeñas modificaciones

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,6 +1,6 @@
 
 export function CartWidget () {
     return(
-        <img src="./img/cartlogo.png" alt="Cart" className="cartLogo"/>
+        <img src="./img/cartlogo.png" alt="Cart" className="CartLogo"/>
     )
 }
